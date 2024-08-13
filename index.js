6e438defe8cc04b5c3c44a5c1ecd3c646ce5661d
Leachenski <jcleachenski@gmail.com>
2024-08-13 11:43:15 -0300

    feat: crud corretora

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
 const express = require('express')
+const corretora = require('./controllers/corretora')
 const app = express()
 const port = 5000
 
 app.use(express.json())
 
-app.get("/:usuario/:repositorio", (req, res) => {
+/* Como pegar os dados que vem do cliente */
+// app.get("/:usuario/:repositorio", (req, res) => {
     
-    console.log("antes do ?:", req.params.usuario, req.params.repositorio)
-    console.log("depois do ?:", req.query.id, req.query.idade)
-    console.log("corpo da requisição: ", req.body.cidade, req.body.bairro)
+//     console.log("antes do ?:", req.params.usuario, req.params.repositorio)
+//     console.log("depois do ?:", req.query.id, req.query.idade)
+//     console.log("corpo da requisição: ", req.body.cidade, req.body.bairro)
 
-    res.send("Tá frio demais")
+//     res.send("Tá frio demais")
+// })
+/* Como pegar os dados que vem do cliente */
+
+app.get("/corretora", (req, res) => {
+    res.json(corretora.index())
+})
+
+app.post("/corretora", (req, res) => {
+    const code = corretora.store(req.body)
+    res.status(code).json()
+})
+
+app.get("/corretora/:id", (req, res) => {
+    res.json(corretora.show(req.params.id))
+})
+
+app.put("/corretora/:id", (req, res) => {
+    const code = corretora.update(req.body, req.params.id);
+    res.status(code).json()
+})
+
+app.delete("/corretora/:id", (req, res) => {
+    const code = corretora.destroy(req.params.id)
+    res.status(code).json()
 })
 
 app.listen(port, () => {
